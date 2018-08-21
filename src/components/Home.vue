1ba681e5c2413efb4001dc213c4265cc336b3849
phanudet2016 <5706021622141@fitm.kmutnb.ac.th>
2018-08-21 20:03:26 +0700

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,97 @@
+<template>
+  <div>
+    <!-- <nav class="navbar navbar-light" style="background-color:#3d3d3d;height:31px;">
+      <a class="navbar-brand" style="color:#ffffff;font-size:12px;">phanudetkawilai2016@gmail.com</a>
+    </nav> -->
+    <nav class="navbar navbar-expand-lg navbar-light" style="border-bottom:1px solid #dddddd;background-color:#ffffff;">
+      <div class="navbar-brand">
+        <router-link to="/" style="text-decoration: none;">
+          <a style="color:rgb(60, 60, 60);font-size:30px;text-decoration: none;">Phanudet</a>
+        </router-link>
+      </div>
+      <button style="border:none;" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <!-- <span><img src="../assets/menu_btn.png" width="37" height="37"></span> -->
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <a class="mr-auto"></a>
+        <ul class="navbar-nav">
+          <router-link to="/">
+            <li class="nav-item active" id="Hover">
+              <a class="nav-link">Home <span class="sr-only">(current)</span></a>
+            </li>
+          </router-link>
+          <router-link to="/resume">
+            <li class="nav-item" id="Hover">
+              <a class="nav-link">Resume</a>
+            </li>
+          </router-link>
+          <router-link to="/CV">
+            <li class="nav-item" id="Hover">
+              <a class="nav-link">CV</a>
+            </li>
+          </router-link>
+          <router-link to="/Grade">
+            <li class="nav-item" id="Hover">
+              <a class="nav-link">Grade</a>
+            </li>
+          </router-link>
+          <router-link to="/Contact">
+            <li class="nav-item" id="Hover">
+              <a class="nav-link">Contact</a>
+            </li>
+          </router-link>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+.navbar-brand {
+  margin-left: 230px;
+}
+.navbar-nav {
+  font-size:18px;
+  margin-right: 230px;
+}
+
+#Hover a:hover{
+  color: rgb(60, 135, 199);
+}
+
+@media (max-width: 980px) {
+  .navbar-brand {
+    margin-left:20px;
+  }
+  .navbar-nav {
+    font-size:18px;
+    margin-right: auto;
+  }
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/components/Home'
+import Resume from '@/components/Resume'
+import CV from '@/components/CV'
+import Grade from '@/components/Grade'
+import Contact from '@/components/Contact'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/resume',
+      name: 'resume',
+      component: Resume
+    },
+    {
+      path: '/cv',
+      name: 'cv',
+      component: CV
+    },
+    {
+      path: '/grade',
+      name: 'grade',
+      component: Grade
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: Contact
+    }
+  ]
+})
